6d94d7314db832fc5d8dc45aa94f8afba25bc6d7
Shanmukh459 <saikrrish459@gmail.com>
2024-02-29 13:28:01 -0800

    restructures Home and About components to pages folder

---
+++ pages/About.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function About() {
+  return (
+    <>
+      <nav>
+        <Link className="home-btn" to="/">#VanLife</Link>
+        <Link className="vans-btn" to="/vans">Vans</Link>
+        <Link className="about-btn" to="/about">About</Link>
+      </nav>
+      <img className="about-hero" src="./about.png" />
+      <section className="about-sec">
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van rental. Our vans are   recertified before each trip to ensure your travel plans can go off without a hitch.
+          (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
+        </p>
+        <div className="about-card">
+          <h2>Your destination is waiting. Your van is ready.</h2>
+          <button>Explore our vans</button>
+        </div>
+      </section>
+      <footer>
+        <p>&copy; 2024 #VANLIFE</p>
+      </footer>
+    </>
+  )
+}
