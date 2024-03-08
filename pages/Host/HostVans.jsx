8eac831a2087dc289c0d879be3b09464a265fb39
Shanmukh459 <saikrrish459@gmail.com>
2024-03-08 11:23:28 -0800

    added host/vans page data

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,7 +1,30 @@
 import React from "react"
 
 export default function HostVans() {
+    const [vans, setVans] = React.useState([])
+    
+    React.useEffect(() => {
+        fetch("/api/host/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+    
+    const vanElements = vans.map((van) => {
+        return (
+            <div key={van.name} className="host-van-card">
+                <img src={van.imageUrl} />
+                <div>
+                    <p className="host-van-card-title">{van.name}</p>
+                    <p className="host-van-card-price">${van.price}/day</p>
+                </div>
+            </div>
+        )
+    })
+    
     return (
-        <h1>Host vans page!</h1>
+        <div className="host-vans-container">
+            <h1>Your listed vans</h1>
+            {vanElements}
+        </div>
     )
 }
